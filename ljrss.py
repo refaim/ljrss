10920cb62b76857c189c36f92790ab31f6673dff
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-11 13:00:52 +1100

    Initial commit

---
+++ ljrss.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+__author__ = 'Roman Kharitonov (refaim.vl@gmail.com)'
+__license__ = 'GPLv2'
+
+import sys
+import re
+import xml.dom.minidom
+import mechanize
+
+import lj
+from pbar import ProgressBar
+
+FMF_URL = 'http://freemyfeed.com/'
+FMF_FORM_ID = 0
+LJ_RSS_URL = 'http://livejournal.com/users/{0}/data/rss'
+
+class Config(object):
+    def __init__(self):
+        self.user = None
+        self.password = None
+
+def error(message):
+    message = 'Error: {0!s}'.format(message)
+    print(message)
+    return 1
+
+config = Config()
+
+def main(argv):
+    if len(argv) > 3:
+        return error('ljrss.py <lj-username> <password> [filename]')
+    config.user, config.password = argv[0], argv[1]
+    config.filename = argv[2] if len(argv) == 3 else 'lj_mutual.xml'
+
+    print('Getting the list of mutual friends...')
+    try:
+        livejournal = lj.LJServer('lj.py; kemayo@gmail.com', 'Python-PyLJ/0.0.1')
+        livejournal.login(config.user, config.password)
+        
+        def getusernames(response):
+            return (item['username'] for item in response)
+        
+        friendof = getusernames(livejournal.friendof()['friendofs'])
+        friends = getusernames(livejournal.getfriends()['friends'])
+        mutualfriends = list(set(friendof) & set(friends))
+    except lj.LJException as e:
+        return error(e)
+    print('User {0} has {1} mutual friends'.format(config.user, len(mutualfriends)))
+
+    browser = mechanize.Browser()
+    
+    def getrssurl(url):
+        browser.open(FMF_URL)
+        browser.select_form(nr=FMF_FORM_ID)
+        browser['url'] = url
+        browser['user'] = config.user
+        browser['pass'] = config.password
+        response = browser.submit()
+    
+        url_re = r'\<p id="viewrss"\>\<a href="(.*)" onclick.*\<\/p\>'
+        return re.search(url_re, response.read()).group(1)
+
+    data = ((username, getrssurl(LJ_RSS_URL.format(username))) for username in mutualfriends)
+
+    document = xml.dom.minidom.Document()
+    opml = document.createElement('opml')
+    opml.setAttribute('version', '1.1')
+    document.appendChild(opml)
+    body = document.createElement('body')
+    opml.appendChild(body)
+    folder = document.createElement('outline')
+    folder.setAttribute('text', 'lj_mutual')
+    body.appendChild(folder)
+
+    print('Working with FreeMyFeed...')
+    progressbar = ProgressBar(maxval=len(mutualfriends))
+    for username, url in data:
+        entry = document.createElement('outline')
+        entry.setAttribute('title', username)
+        entry.setAttribute('text', username)
+        entry.setAttribute('xmlUrl', url)
+        folder.appendChild(entry)
+        progressbar.update(1)
+    progressbar.finish()
+
+    print('Creating OPML-file...')
+    with open(config.filename, 'w') as opmlfile:
+        opmlfile.write(document.toprettyxml(indent=' ' * 2))
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
